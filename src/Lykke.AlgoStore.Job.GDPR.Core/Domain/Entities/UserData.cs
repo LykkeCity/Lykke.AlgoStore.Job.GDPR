93489ff8c7939e932d7300873be0308094ab6964
Todor Ivanov <todor.ivanov@primeholding.com>
2018-08-16 11:57:40 +0300

    Add deletionStatus property

--- src/Lykke.AlgoStore.Job.GDPR.Core/Domain/Entities/UserData.cs
+++ src/Lykke.AlgoStore.Job.GDPR.Core/Domain/Entities/UserData.cs
@@ -9,5 +9,13 @@ namespace Lykke.AlgoStore.Job.GDPR.Core.Domain.Entities
         public string ClientId { get; set; }
         public bool GDPRConsent { get; set; }
         public bool CookieConsent { get; set; }
+        public DeletionStatus DeletionStatus { get; set; }
+    }
+
+    public enum DeletionStatus
+    {
+        None = 0,
+        InProgress = 1,
+        Done = 2
     }
 }
