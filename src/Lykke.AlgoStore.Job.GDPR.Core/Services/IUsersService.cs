ac97c88479a3b55f9a9b4a8e879ab88d69fdaed6
Todor Ivanov <todor.ivanov@primeholding.com>
2018-08-16 11:34:02 +0300

    Add GDPR logic

---
+++ src/Lykke.AlgoStore.Job.GDPR.Core/Services/IUsersService.cs
@@ -0,0 +1,17 @@
+﻿using Lykke.AlgoStore.Job.GDPR.Core.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lykke.AlgoStore.Job.GDPR.Core.Services
+{
+    public interface IUsersService
+    {
+        Task SeedAsync(string clientId);
+        Task<UserData> GetByIdAsync(string clientId);
+        Task SetGDPRConsentAsync(string clientId);
+        Task SetCookieConsentAsync(string clientId);
+        Task DeactivateAccountAsync(string clientId);
+    }
+}
