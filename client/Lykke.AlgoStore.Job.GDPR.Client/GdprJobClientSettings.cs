ed5d8cdb0549adde77e72a329b79280943120789
Miladin Joksic <miladin.joksic@primeholding.com>
2018-08-21 13:06:14 +0200

    Added GDPR client settings

---
+++ client/Lykke.AlgoStore.Job.GDPR.Client/GdprJobClientSettings.cs
@@ -0,0 +1,14 @@
+﻿using Lykke.SettingsReader.Attributes;
+
+namespace Lykke.AlgoStore.Job.GDPR.Client
+{
+    /// <summary>
+    /// Gdpr job client settings
+    /// </summary>
+    public class GdprJobClientSettings
+    {
+        /// <summary>Service url.</summary>
+        [HttpCheck("api/isalive")]
+        public string ServiceUrl { get; set; }
+    }
+}
