64c029e8756dfe68162324516730f12b8375d3cb
Miladin Joksic <miladin.joksic@primeholding.com>
2018-09-10 10:27:40 +0200

    Updated nuget packages to latest versions

--- src/Lykke.AlgoStore.Job.GDPR/Startup.cs
+++ src/Lykke.AlgoStore.Job.GDPR/Startup.cs
@@ -76,9 +76,12 @@ namespace Lykke.AlgoStore.Job.GDPR
 
                 services.AddSwaggerGen(options => { options.DefaultLykkeConfiguration(ApiVersion, ApiName); });
 
-                var settingsManager = Configuration.LoadSettings<AppSettings>(x => (
-                    x.SlackNotifications.AzureQueue.ConnectionString, x.SlackNotifications.AzureQueue.QueueName,
-                    $"{AppEnvironment.Name} {AppEnvironment.Version}"));
+                var settingsManager = Configuration.LoadSettings<AppSettings>(x =>
+                {
+                    x.SetConnString(y => y.SlackNotifications.AzureQueue.ConnectionString);
+                    x.SetQueueName(y => y.SlackNotifications.AzureQueue.QueueName);
+                    x.SenderName = $"{AppEnvironment.Name} {AppEnvironment.Version}";
+                });
 
                 var appSettings = settingsManager.CurrentValue;
 
